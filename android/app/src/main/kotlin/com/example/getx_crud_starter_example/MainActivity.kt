65dc166e9be343db6ab812dd8b8d35fdc824d98e
Reza <sabekur2017@gmail.com>
2023-01-12 00:26:43 +0600

    crud app stated

---
+++ android/app/src/main/kotlin/com/example/getx_crud_starter_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getx_crud_starter_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:getx_crud_starter_example/presentation/app.dart';
+
+void main() {
+  runApp(App());
+}
